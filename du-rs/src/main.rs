d54e0ee5848cf7df1b11bd1e3a2c9fc8e572cda4
vamsi200 <vamsivanama891@gmail.com>
2025-02-28 21:20:35 +0530

    added argument functionality and corrected some logical errors

--- du-rs/src/main.rs
+++ du-rs/src/main.rs
@@ -3,41 +3,14 @@
 #![allow(unused_variables)]
 use nix::fcntl::OFlag;
 use nix::{sys::stat::Mode, *};
-use std::clone;
 use std::collections::{BTreeMap, HashMap, VecDeque};
+use std::env::args;
 use std::os::fd::AsRawFd;
 use std::path::{Path, PathBuf};
+use std::{clone, str};
 use std::{env, error::Error, result};
 
 type MyResult<T> = result::Result<T, Box<dyn Error>>;
-
-//fn scan_directory(dir: &Path) -> HashMap<PathBuf, String> {
-//    //Returns Full path, i.e.. PathBuf and a string saying that its a file or directory
-//    let open_dir = nix::dir::Dir::open(dir, OFlag::O_RDONLY, Mode::empty()).unwrap();
-//    let mut output = HashMap::new();
-//    for res in open_dir {
-//        match res {
-//            Ok(entry) => {
-//                let file_name = entry.file_name().to_string_lossy();
-//                if file_name == "." && file_name.len() == 1
-//                    || file_name == ".." && file_name.len() == 2
-//                {
-//                    continue;
-//                }
-//                let full_path = dir.join(entry.file_name().to_string_lossy().as_ref());
-//
-//                let file_type = match entry.file_type() {
-//                    Some(nix::dir::Type::Directory) => "dir".to_string(),
-//                    Some(nix::dir::Type::File) => "file".to_string(),
-//                    _ => "unknown".to_string(),
-//                };
-//                output.insert(full_path, file_type);
-//            }
-//            Err(e) => eprintln!("Error {}", e),
-//        }
-//    }
-//    output
-//}
 
 fn get_file_size_in_bytes(file_path: &PathBuf) -> i64 {
     //Returns file size in bytes
@@ -78,11 +51,12 @@ fn get_file_size(file_path: Option<&Path>, bytes: Option<i64>) -> FileSize {
     FileSize { bytes, formatted }
 }
 
-fn format_file_size(file_path: &Path, arg: String) -> MyResult<String> {
-    //Returns file size based on argument given
-    if let Ok(res) = nix::sys::stat::stat(file_path) {
+fn format_file_size(file_map: &BTreeMap<PathBuf, Vec<PathBuf>>, arg: &str) -> MyResult<String> {
+    for (dir_path, _files) in file_map {
+        let res = nix::sys::stat::stat(dir_path).map_err(|_| "Failed to get file size")?;
         let bytes = res.st_blocks * 512;
-        let output = match arg.as_str() {
+
+        let output = match arg {
             "BT" => format!("{}T", (bytes as f64 / 1_099_511_627_776.0).ceil()),
             "BP" => format!("{}P", (bytes as f64 / 1_125_899_906_842_624.0).ceil()),
             "BE" => format!("{}E", (bytes as f64 / 1_152_921_504_606_846_976.0).ceil()),
@@ -104,56 +78,136 @@ fn format_file_size(file_path: &Path, arg: String) -> MyResult<String> {
             ),
             _ => return Err("-B Requires a valid argument".into()),
         };
+
         return Ok(output);
     }
-    Err("Failed to get file size".into())
+
+    Err("No valid directories found".into())
 }
 
 fn count_files(dir: &Path) -> u64 {
     todo!()
 }
 
-fn calculate_total_dir_size(dir: BTreeMap<PathBuf, Vec<PathBuf>>) {
-    let mut total: i64 = 0;
+fn calculate_total_dir_size<F>(
+    dir: &BTreeMap<PathBuf, Vec<PathBuf>>,
+    format: bool,
+    mut output_fn: F,
+) -> i64
+where
+    F: FnMut(&str),
+{
     let mut summarize_size: i64 = 0;
     let current_dir = env::current_dir().unwrap();
-    for (dir_path, files) in dir.clone() {
-        match dir.get(&dir_path) {
-            Some(d) => {
-                for file in d {
-                    let fb = get_file_size(Some(file), None);
-                    let bytes = fb.bytes;
-                    let f_bytes = fb.formatted;
-                    let stripped_file = file
-                        .strip_prefix(current_dir.clone())
-                        .unwrap_or(file)
-                        .to_path_buf();
-                    println!("{:<10}./{}", f_bytes, stripped_file.display());
-                    total += bytes;
-                }
-            }
-            None => println!("wut?"),
-        }
-        let total_dir_formatted = get_file_size(None, Some(total));
-        let tdf = total_dir_formatted.formatted;
-        summarize_size += total_dir_formatted.bytes;
 
-        println!("{:<10}{}", tdf, dir_path.display());
+    for (dir_path, files) in dir {
+        let mut dir_total: i64 = 0;
+
+        for file in files {
+            let file_size = get_file_size(Some(file), None);
+            let stripped_file = file.strip_prefix(&current_dir).unwrap_or(file);
+
+            let output = if format {
+                format!("{:<10} ./{}", file_size.formatted, stripped_file.display())
+            } else {
+                format!("{:<10} ./{}", file_size.bytes, stripped_file.display())
+            };
+
+            output_fn(&output);
+            dir_total += file_size.bytes;
+        }
+
+        let total_dir_formatted = get_file_size(None, Some(dir_total));
+
+        let output = if format {
+            format!(
+                "{:<10} {}",
+                total_dir_formatted.formatted,
+                dir_path.display()
+            )
+        } else {
+            format!("{:<10} {}", dir_total, dir_path.display())
+        };
+
+        output_fn(&output);
+        summarize_size += dir_total;
     }
-    let something = get_file_size(None, Some(summarize_size));
-    let s = something.formatted;
-    println!("Entirety : {}", s);
+
+    summarize_size
 }
 
-//fn scan_directory_recursive(dir: &Path) {
-//    let output = scan_directory(&dir);
-//    for (path, file_type) in output {
-//        if file_type == "dir" {
-//            println!("\nDirectory: {:?}", path);
-//            scan_directory_recursive(&path);
-//        }
-//    }
-//}
+#[derive(Debug)]
+struct Args {
+    path: PathBuf,
+    human_readable: bool,
+    depth: Option<i32>,
+    summarize: bool,
+    bytes: bool,
+    total: bool,
+    block_size: Option<String>,
+    threshold: Option<u64>,
+    x: Option<PathBuf>,
+    xclude: Option<PathBuf>,
+}
+
+fn handle_args() -> Args {
+    let mut arguments = env::args().skip(1);
+    let mut path = env::current_dir().unwrap();
+    let mut human_readable = false;
+    let mut depth = None;
+    let mut summarize = false;
+    let mut bytes = false;
+    let mut total = false;
+    let mut block_size = None;
+    let mut threshold = None;
+    let mut x = None;
+    let mut xclude = None;
+
+    while let Some(arg) = arguments.next() {
+        match arg.as_str() {
+            "-h" | "--human_readable" => human_readable = true,
+            "-b" => bytes = true,
+            "-s" | "--summarize" => summarize = true,
+            "-c" | "--total" => total = true,
+            "-d" | "--max-depth" => {
+                if let Some(val) = arguments.next() {
+                    depth = val.parse::<i32>().ok();
+                }
+            }
+            _ if arg.starts_with("-B") => {
+                block_size = arg.strip_prefix("-").map(|s| s.to_string());
+            }
+            "-t" | "--threshold" => {
+                if let Some(val) = arguments.next() {
+                    threshold = val.parse::<u64>().ok();
+                }
+            }
+            "-x" | "--one-file-system" => {
+                if let Some(val) = arguments.next() {
+                    x = val.parse::<PathBuf>().ok();
+                }
+            }
+            "-X" | "--exclude-from" => {
+                if let Some(val) = arguments.next() {
+                    xclude = val.parse::<PathBuf>().ok();
+                }
+            }
+            _ => path = env::current_dir().unwrap(),
+        }
+    }
+    Args {
+        depth,
+        path,
+        human_readable,
+        bytes,
+        summarize,
+        total,
+        block_size,
+        threshold,
+        xclude,
+        x,
+    }
+}
 
 fn scan_directory_iter(root_dir: &Path, max_depth: i32) -> BTreeMap<PathBuf, Vec<PathBuf>> {
     let mut dir_stack = VecDeque::new();
@@ -251,8 +305,40 @@ fn main() {
     //        //println!(".{:?}", d);
     //    }
     //}
+    let g_args = handle_args();
     let dir_map = scan_directory_iter(&current_dir, 0);
-    calculate_total_dir_size(dir_map.clone());
+    //match g_args.depth {
+    //    Some(a) => {
+    //        dir_map = scan_directory_iter(&current_dir, a);
+    //        calculate_total_dir_size(dir_map.clone(), true);
+    //    }
+    //    None => {}
+    //}
+    //calculate_total_dir_size(dir_map.clone(), true);
+    //
+    //match g_args.block_size {
+    //    Some(s) => {
+    //        let out = format_file_size(&dir_map, s.as_str());
+    //        match out {
+    //            Ok(s) => println!("{s}"),
+    //            Err(e) => eprintln!("{}", e),
+    //        }
+    //    }
+    //    None => {}
+    //}
+    let out = calculate_total_dir_size(&dir_map.clone(), g_args.human_readable, |l| {
+        if !g_args.summarize {
+            println!("{}", l);
+        }
+    });
+
+    if g_args.summarize && g_args.human_readable {
+        let output = get_file_size(None, Some(out));
+        println!("{:<10} .", output.formatted);
+    } else {
+        println!("{:<10} .", out);
+    }
+
     //let base_path = current_dir;
     //for (dir, files) in &dir_map {
     //    for file in files {
